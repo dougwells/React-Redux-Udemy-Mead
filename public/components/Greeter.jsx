a33a6113b732aacf52a3cd0fb1615316c89cb386
dougwells <dougwells@gmail.com>
2016-05-30 11:54:52 -0600

    Greeter module in own file

--- public/components/Greeter.jsx
+++ public/components/Greeter.jsx
@@ -1,5 +1,35 @@
-function greeter (){
-  document.write("Good Morning World! from greeter function.  It is 7AM");
-}
+var React = require('react');
+var ReactDOM = require('react-dom');
+var GreeterMessage = require ('./GreeterMessage');
+var GreeterForm = require ('./GreeterForm');
 
-module.exports = greeter;
+var Greeter = React.createClass({
+  getDefaultProps: function () {
+    return {
+      name: 'React',
+      message: 'This is the default message!'
+    };
+  },
+  getInitialState: function () {
+    return {
+        name: this.props.name,
+        message: this.props.message
+    };
+  },
+  handleNewData: function (updates) {
+    this.setState(updates);
+  },
+  render: function () {
+    var name = this.state.name;
+    var message = this.state.message;
+
+    return (
+      <div>
+        <GreeterMessage name={name} message={message}/>
+        <GreeterForm onNewData={this.handleNewData}/>
+      </div>
+    );
+  }
+});
+
+module.exports = Greeter;
