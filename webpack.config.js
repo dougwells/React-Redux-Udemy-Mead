eef429df9767ae1ca869d2bb18985ae1c60b2a6f
dougwells <dougwells@gmail.com>
2016-05-30 07:43:30 -0600

    Added Bable to Webpack Configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,28 @@
 module.exports = {
-  entry: './public/app.js',
+  entry: './public/app.jsx',
   output: {
     path: __dirname,
     filename: './public/bundle.js'
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
+  },
+
+// Need this section to run JSX.
+    //convert react files to ES2015
+    // Do this for any file that ends in .jsx
+    // Exclude node_modules & bower_components
+
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        },
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/
+      }
+    ]
   }
 };
